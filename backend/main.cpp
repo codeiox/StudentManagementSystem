5cb086aa920f015291a10b0cb13d7f0622b11370
OCEAN RAI <sagar.rai2023@outlook.com>
2025-09-16 15:25:01 -0400

    Remove .idea folder from repo

--- backend/main.cpp
+++ backend/main.cpp
@@ -6,19 +6,18 @@ int main(int argc, const char* argv[]) {
     using namespace drogon;
 
 
+
+
+
     const char* envPort = std::getenv("PORT");
     uint16_t port = envPort ? std::stoi(envPort) : 8080;
-
     // tell drogon where your static files live INSIDE the container
-    // (Dockerfile copies ./pages -> /pages)
-    app().setDocumentRoot("/pages");
-
+    // (Dockerfile copies ./public -> /public)
+    app().setDocumentRoot("/public");
     // bind to all interfaces so Docker can forward traffic into the container
     app().addListener("0.0.0.0", port);
-
+    std::cout << "http://localhost:8080/index.html" << std::endl;
     // start the server (blocks here)
     app().run();
-
-
     return 0;
 }
